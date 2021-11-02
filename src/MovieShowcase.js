6f2613af8e5ec15428fff005e8cdcae0ab3ce00d
Sarah <sarahlchavez@gmail.com>
2021-11-02 05:51:12 -0700

    commit tree 512b48c76a406fb0bc39e6656fa95968a2613768

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
+//MovieShowcase has access to movieData by importing this:
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
+  //For every object inside movieData, render a MovieCard component
+  //pass in the object data as props (pass in all 4 props)
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
+    // map over your movieData array and return an array of the correct JSX  
+      return movieData.map(movie => {
+        return (
+          <MovieCard
+            title={movie.title} 
+            IMDBRating={movie.IMDBRating}
+            genres={movie.genres} 
+            poster={movie.poster}
+          />
+        );
+      });
+    }
 
   render() {
     return (
